be4426a63f7f2117b59ca09e94b90790947f0624
dimalei <dima.leigr@gmail.com>
2024-09-15 11:43:47 +0200

    wifimanager works

--- wifi-clock/src/main.cpp
+++ wifi-clock/src/main.cpp
@@ -1,18 +1,24 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include <WiFiManager.h>
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  WiFiManager wm;
+
+  bool res;
+
+  res = wm.autoConnect("WiFi Clock");
+
+  if(!res) {
+    Serial.println("Failed to connect to WiFi");
+    ESP.restart();
+  } else {
+    Serial.println("Connected to WiFi");
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
